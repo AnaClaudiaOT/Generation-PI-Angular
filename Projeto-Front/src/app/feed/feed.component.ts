95484ac624164bb1c281dc04165fbdf6b1fcad23
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2020-09-01 14:23:35 -0300

    Implementar GET de Postagem e Tema.

--- Projeto-Front/src/app/feed/feed.component.ts
+++ Projeto-Front/src/app/feed/feed.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PostagemModel } from '../model/PostagemModel';
+import { TemaModel } from '../model/TemaModel';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postagemService: PostagemService, private temaService: TemaService) { }
 
-  ngOnInit(): void {
+  postagem: PostagemModel = new PostagemModel();
+  listaPostagens: PostagemModel[];
+
+  tema: TemaModel = new TemaModel();
+  listaTemas: TemaModel[];
+  idTema: number;
+
+  ngOnInit() {
+    window.scroll(0,0)
+    this.findAllPostagens()
+    this.findAllTemas()
+  }
+
+  findAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: PostagemModel[]) => {
+      this.listaPostagens = resp;
+    });
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTemas().subscribe((resp: TemaModel[]) => {
+      this.listaTemas = resp;
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: TemaModel) => {
+      this.tema = resp;
+    });
   }
 
 }
